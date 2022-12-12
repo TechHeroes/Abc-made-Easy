99db78a0c03567960d8aa000cc190442cc6705ce
Mansoor Khan <thisismansoorkhan@gmail.com>
2022-12-12 21:08:33 +0530

    Initial commit
    - Added A to Z button click functionality
    - Options to go back and forth and to restart ABCs

--- Abc/ViewController.swift
+++ Abc/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var letters: [String] = [
+        "A","B","C","D","E", "F", "G", "H", "I", "J", "K", "L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"
+    ]
+    var nextKey = 1
+    
+    @IBOutlet weak var letterView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resetButton((Any).self)
     }
 
-
+    @IBAction func prevButton(_ sender: Any) {
+        if nextKey < 1 {
+            nextKey = 1
+        }
+        nextKey -= 1
+        letterView.text = letters[nextKey]
+        
+    }
+    
+    @IBAction func nextButton(_ sender: Any) {
+        
+        if nextKey > 24 {
+            nextKey = 24
+        }
+        nextKey += 1
+        letterView.text = letters[nextKey]
+    }
+    
+    @IBAction func resetButton(_ sender: Any) {
+       letterView.text = letters[0]
+        nextKey = 0
+    }
 }
 
