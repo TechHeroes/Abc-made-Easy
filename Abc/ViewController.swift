181adae122910e94c009ca428193377a85d0042b
Mansoor Khan <thisismansoorkhan@gmail.com>
2022-12-13 09:25:02 +0530

    Add system sound on button tap

--- Abc/ViewController.swift
+++ Abc/ViewController.swift
@@ -6,7 +6,10 @@
 //
 
 import UIKit
+import AVFoundation
 
+@available(iOS 15.0, *)
+@available(iOS 15.0, *)
 class ViewController: UIViewController {
 
     var letters: [String] = [
@@ -14,6 +17,33 @@ class ViewController: UIViewController {
     ]
     var nextKey = 1
     
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemRed,
+        .systemBlue,
+        .systemCyan,
+        .systemMint,
+        .systemTeal,
+        .systemOrange,
+        .systemBrown,
+        .systemIndigo,
+        .systemPurple,
+        .systemYellow
+    ]
+    
+    var player: AVAudioPlayer!
+    // create a sound ID, in this case its the tweet sound.
+    let sounds = [
+        1016, //tweet
+        1009, // SMS received
+        1016, // SMS sent
+        
+    ]
+    let systemSoundID: SystemSoundID = 1211
+    func playSound() {
+        AudioServicesPlaySystemSound(systemSoundID)
+    }
+
     @IBOutlet weak var letterView: UITextView!
     
     override func viewDidLoad() {
@@ -28,7 +58,8 @@ class ViewController: UIViewController {
         }
         nextKey -= 1
         letterView.text = letters[nextKey]
-        
+        letterView.textColor = colors.randomElement()
+        playSound()
     }
     
     @IBAction func nextButton(_ sender: Any) {
@@ -38,7 +69,9 @@ class ViewController: UIViewController {
         }
         nextKey += 1
         letterView.text = letters[nextKey]
-    }
+        letterView.textColor = colors.randomElement()
+        playSound()
+  }
     
     @IBAction func resetButton(_ sender: Any) {
        letterView.text = letters[0]
