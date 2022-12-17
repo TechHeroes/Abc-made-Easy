dcfe68e1ad5e34811dd0c58f318b92c0aa9945e1
Mansoor Khan <thisismansoorkhan@gmail.com>
2022-12-17 22:01:55 +0530

    Add app icons and launch screen, sounds

--- Abc/appOperations.swift
+++ Abc/appOperations.swift
@@ -13,19 +13,7 @@ let letterImagesAndSounds: [String] = Constants.Alphabets.English.letterImagesAn
 let letters: [String] = Constants.Alphabets.English.letters
 @available(iOS 15.0, *)
 let colors: [UIColor] = Constants.Alphabets.English.colors
-let sounds = [
-    1016, //tweet
-    1009, // SMS received
-    1016, // SMS sent
-    
-]
-let systemSoundID: SystemSoundID = 1211
 var player: AVAudioPlayer!
-// create a sound ID, in this case its the tweet sound.
-
-func playSound() {
-    AudioServicesPlaySystemSound(systemSoundID)
-}
 
 func playAudio(index: Int){
     guard (index >= 0) && (index <= 25) else {
@@ -45,6 +33,7 @@ func playAudio(index: Int){
 func setImage(index: Int, target: UIImageView) {
     target.image = UIImage(named: letterImagesAndSounds[index])
 }
+
 @available(iOS 15.0, *)
 func setTextAndColor(index: Int, target: UITextView) {
     target.text = letters[index]
